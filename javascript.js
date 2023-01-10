aaa6e4f93791ced6b2ba42173b3e81d99f9ed49d
Khwalabear <64755825+Khwalabear@users.noreply.github.com>
2023-01-09 20:47:50 -0600

    added notes

--- javascript.js
+++ javascript.js
@@ -1,13 +1,25 @@
 const add_note = document.querySelector('#add-note')
 const notes = document.querySelector(".notes")
+const exit = document.querySelector(".exit")
 //add_note.addEventListener("click", function () {
 //    add_note.style.backgroundColor = 'blue'
 //})
 
 add_note.addEventListener("click", function () {
-    add_note.style.backgroundColor = 'blue'
+    //new_note = prompt("Enter your note:")
+    const exit = document.createElement('div')
     const createDiv = document.createElement('div')
+    exit.setAttribute('class', 'exit')
     createDiv.setAttribute('class', 'note')
-    createDiv.style.backgroundColor = '#edae6f'
+    exit.style.backgroundColor = '#fff'
+    exit.textContent = 'X'
+    createDiv.style.backgroundColor = '#94bca9'
+    createDiv.appendChild(exit)
+    //createDiv.textContent = new_note
     notes.appendChild(createDiv)
+})
+
+exit.addEventListener("click", function () {
+    exit.parentElement.remove()
+    console.log('printed')
 })
