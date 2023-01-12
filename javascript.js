96b34e88b92ce4fe922350575825c2f408cdd828
Khwalabear <64755825+Khwalabear@users.noreply.github.com>
2023-01-11 21:38:19 -0600

    added form

--- javascript.js
+++ javascript.js
@@ -1,25 +1,44 @@
 const add_note = document.querySelector('#add-note')
 const notes = document.querySelector(".notes")
-const exit = document.querySelector(".exit")
-//add_note.addEventListener("click", function () {
-//    add_note.style.backgroundColor = 'blue'
-//})
+const form = document.querySelector("#popup-form")
+    
 
-add_note.addEventListener("click", function () {
-    //new_note = prompt("Enter your note:")
+//click add notes
+const clickNote = () =>{
+    form.style.display = "inline"
+    notes.style.opacity = ".5"
+}
+//Create new sticky notes
+const createNotes = () =>{
+    event.preventDefault()
+    form.style.display = "none"
+    notes.style.opacity = "1"
+    userInput = document.forms["sticky-note"]["inputNotes"].value
     const exit = document.createElement('div')
-    const createDiv = document.createElement('div')
+    const para = document.createElement('p')
     exit.setAttribute('class', 'exit')
+    const createDiv = document.createElement('div')
     createDiv.setAttribute('class', 'note')
     exit.style.backgroundColor = '#fff'
     exit.textContent = 'X'
     createDiv.style.backgroundColor = '#94bca9'
+    para.textContent = userInput
     createDiv.appendChild(exit)
-    //createDiv.textContent = new_note
+    createDiv.appendChild(para)
     notes.appendChild(createDiv)
-})
+    exitNotes()
+}
+//Delete existing sticky notes
+const exitNotes = () =>{
+    const exits =  document.querySelectorAll(".exit")
+    exits.forEach((e)=>{
+        e.addEventListener("click", function () {
+            e.parentElement.remove()
+        })
+    })
+}
 
-exit.addEventListener("click", function () {
-    exit.parentElement.remove()
-    console.log('printed')
-})
+
+add_note.addEventListener("click", clickNote)
+
+
