6ae0a43b4928422bad187ce7034882eea51211e9
Khwalabear <64755825+Khwalabear@users.noreply.github.com>
2023-01-19 22:24:53 -0600

    added edit functionality

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const add_note = document.querySelector('#add-note')
 const notes = document.querySelector(".notes")
 const form = document.querySelector("#popup-form")
+const edit = document.querySelector("#edit")
     
 
 //click add notes
@@ -11,22 +12,36 @@ const clickNote = () =>{
 //Create new sticky notes
 const createNotes = () =>{
     event.preventDefault()
+    const userInput = document.forms["sticky-note"]["inputNotes"].value
     form.style.display = "none"
     notes.style.opacity = "1"
-    userInput = document.forms["sticky-note"]["inputNotes"].value
-    const exit = document.createElement('div')
-    const para = document.createElement('p')
-    exit.setAttribute('class', 'exit')
     const createDiv = document.createElement('div')
     createDiv.setAttribute('class', 'note')
-    exit.style.backgroundColor = '#fff'
-    exit.textContent = 'X'
     createDiv.style.backgroundColor = '#94bca9'
-    para.textContent = userInput
+    const exit = document.createElement('div')
+    const para = document.createElement('div')
+    const edit = document.createElement('div')
+    exit.setAttribute('class', 'exit')
+    edit.setAttribute('class', 'editbtn')
+    exit.textContent = 'X'
+    edit.textContent = 'Edit'
+    exit.style.backgroundColor = '#fff'
+    para.innerHTML = `<p>${userInput}</p>`
     createDiv.appendChild(exit)
     createDiv.appendChild(para)
+    createDiv.appendChild(edit)
     notes.appendChild(createDiv)
     exitNotes()
+    editNotes()
+
+}
+const updateNote = (editNote) => {
+    event.preventDefault()
+    const userInput = document.forms["edit-form"]["editNotes"].value
+    edit.style.display = "none"
+    notes.style.opacity = "1"
+    editNote.children[1].innerHTML = `<p>${userInput}</p>`
+    console.log(editNote)
 }
 //Delete existing sticky notes
 const exitNotes = () =>{
@@ -36,6 +51,19 @@ const exitNotes = () =>{
             e.parentElement.remove()
         })
     })
+}
+
+const editNotes = () =>{
+    event.preventDefault()
+    const editForm =  document.querySelectorAll(".editbtn")
+    const userInput = document.forms["edit-form"]["editNotes"].value
+        editForm.forEach((e)=>{
+            e.addEventListener("click", function () {
+                edit.style.display = "inline"
+                notes.style.opacity = ".5"
+                document.getElementById("save").onsubmit = function(){updateNote(e.parentElement)}
+            })
+        })
 }
 
 
